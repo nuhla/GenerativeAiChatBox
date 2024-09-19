4075610854012cf9313e10c78c5b9dd2dbd05844
nuhlamasri@gmai.com <nuhlamasri@gmail.com>
2024-09-19 21:57:27 +0300

    Setup Pakeage

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='mcqgeneratore',
+    version='0.0.1',
+    author='Nahla.I.M',
+    author_email="NUHLAMASSRI@GMAAIL.COM",
+    install_requires=['','','','','',''],
+    packages=find_packages()
+
+)
